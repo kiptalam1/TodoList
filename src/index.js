3429d4bc6ae24ddc2d98dc72212d2b8380667a3c
adams <adamskiptalam0@gmail.com>
2024-12-03 20:53:31 +0300

    Add task manager to the sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import "./styles.css";
 import "./modal.css";
 import { unchecked } from "./Assets/unchecked.png";
-import { UpdateTaskDisplay } from "./taskDisplay.js";
 
-// getInput();
-// document.addEventListener("DOMContentLoaded", () => {
-//     UpdateTaskDisplay.initialize();
-// });
